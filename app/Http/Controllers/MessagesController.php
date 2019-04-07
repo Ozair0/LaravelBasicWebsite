372d3c835606901141c97a0647310ee061cbc533
Ozair <ozairfarahi6@gmail.com>
2019-04-07 11:19:29 +0430

    Made a model for the database as will now data can be submited to the database using the model

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -12,7 +13,19 @@ class MessagesController extends Controller
            'name' => 'required',
             'email' => 'required',
         ]);
-        return 'SUCCESS';
+
+        // Create a new Message in Database
+
+        $message = new Message();
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+
+        // Save Message
+        $message->save();
+
+        // redirect to home
+        return redirect('/')->with('success', 'Message Sent!');
     }
 
 }
